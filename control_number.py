575fe020b5d5dd8095498fd83e9fd094a30bd48d
Vicariss <54385815+Vicariss@users.noreply.github.com>
2022-07-05 19:47:37 +0200

    Add files via upload

---
+++ control_number.py
@@ -0,0 +1,28 @@
+"""Script for calculating control number of Land and Mortgage Registry."""
+
+# create multipliers list [1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7]
+multipliers = [n for n in range(1,9,2) if n != 5] * 4
+ascii_dict = {
+            88: '10', 65: '11', 66: '12', 67: '13', 68: '14', 69: '15', 70: '16',
+            71: '17', 72: '18', 73: '19', 74: '20', 75: '21', 76: '22', 77: '23',
+            78: '24', 79: '25', 80: '26', 82: '27', 83: '28', 84: '29', 85: '30',
+            87: '31', 89: '32', 90: '33'
+            }
+
+
+def get_control_number(land_reg_id, court):
+    """Calculate control number based on land and mortgage register number and court department code."""
+    combination = []
+    for value in court:
+        translated = value.translate(ascii_dict)  # changing each value from court code
+        combination.append(int(translated))  # adding encoded values to combination list
+
+    splitted = [int(i) for i in str(land_reg_id)]  # splitting the id
+    for number in splitted:
+        combination.append(number)  # adding every number to the combination from previously splitted id
+
+    result = [combination[i] * multipliers[i] for i in range(len(combination))]  # multiplying list by list of multipliers
+
+    to_mod = sum(result)  # summing list
+    control_number = to_mod % 10
+    return control_number
